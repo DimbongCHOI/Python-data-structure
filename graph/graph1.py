a8b3188d27e8871bfd065ba954060a880968d1d8
DImbongCHOI <cgc0328@gmail.com>
2025-03-23 06:29:35 +0000

    feat: 자료구조 폴더 구조 및 예시 추가

---
+++ graph/graph1.py
@@ -0,0 +1,4 @@
+# graph1.py - Graph 실습 코드
+def graph_example():
+    graph = {1: [2, 3], 2: [4], 3: [4], 4: []}
+    print(graph)
---
+++ hash/hash1.py
@@ -0,0 +1,4 @@
+# hash1.py - Hash 실습 코드
+def hash_example():
+    my_dict = {'key': 'value'}
+    print(my_dict['key'])
