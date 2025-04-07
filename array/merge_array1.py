f80eae2bf725e11ec4f04e1557a72c317dd007bf
DimbongCHOI <cgc0328@gmail.com>
2025-04-07 09:18:59 +0000

    feat: array 폴더에 시간복잡도 별 병합 알고리즘 추가

---
+++ array/merge_array1.py
@@ -0,0 +1,26 @@
+# ver1.1 - Array : bubble sort -> O(n^2)..  기준 부적격
+list1 = [3, 5, 10, 21]
+list2 = [4, 6, 15, 26, 33]
+list3 = list1 + list2
+
+for i in range(len(list3)): # -> O(n^2)..  기준 부적격
+    for j in range(len(list3) - i - 1):
+        if(list3[j] > list3[j + 1]):
+            tmp = list3[j]
+            list3[j] = list3[j + 1]
+            list3[j + 1] = tmp
+
+print("Input:")
+print("list1:", end =" ")
+for i in range(len(list1)):
+    print(list1[i], end = ' -> ')
+print("끝")
+print("list2:", end =" ")
+for i in range(len(list2)):
+    print(list2[i], end = ' -> ')
+print("끝")
+
+print("Output:")
+for i in range(len(list3)):
+    print(list3[i], end = ' -> ')
+print("끝")
