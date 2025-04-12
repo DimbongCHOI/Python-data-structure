8c6f8c84b086bf781a3514dfa52bd63015a06c49
DimbongCHOI <cgc0328@gmail.com>
2025-04-12 04:47:20 +0000

    이중 연결 리스트 함수

---
+++ array/Doubly_LinkedList.py
@@ -0,0 +1,31 @@
+class DoubleNode:
+    def __init__(self, data):
+        self.data = data
+        self.linkLeft = None  # 이전 노드
+        self.linkRight = None  # 다음 노드
+
+class DoublyLinkedList: # 이중 연결 리스트
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def append(self, data):
+        pass  
+
+    def printForward(self):
+        pass 
+
+    def printBackward(self):
+        pass  
+
+    def insert(self, index, data):
+        pass
+
+    def delete(self, index):
+        pass
+
+    def search(self, data):
+        pass
+
+    def reverse(self):
+        pass
