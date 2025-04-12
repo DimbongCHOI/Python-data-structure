55c461cab5e943a40298ab96018d509c3b8cbdf4
DimbongCHOI <cgc0328@gmail.com>
2025-04-12 04:47:22 +0000

    순회 연결 리스트 함수

---
+++ array/Circular_LinkedList.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.link = None
+
+class CircularLinkedList: # 순회 연결 리스트
+    def __init__(self):
+        self.head = None
+        self.tail = None  # 편의를 위해 tail도 관리함
+
+    def append(self, data):
+        pass
+
+    def printList(self):
+        pass
+
+    def insert(self, index, data):
+        pass
+
+    def delete(self, index):
+        pass
+
+    def search(self, data):
+        pass
